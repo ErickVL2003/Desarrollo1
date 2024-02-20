f0accf77e9c90c8bffcd860fc049b8c896b29b17
ErickVL2003 <125925743+ErickVL2003@users.noreply.github.com>
2024-02-19 21:19:43 -0500

    Add files via upload
    ahora si

---
+++ DESARROLLOUNO_ERICKVALENZUELALOPEZ/src/LIBRERIA/funciones.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LIBRERIA;
+
+/**
+ *
+ * @author erick
+ */
+public class funciones {
+    public double areaT(double a, double b){
+        return((b*a)/2);
+    }
+    public int perimetroC(int a, int b){
+        return (a*4);
+    }
+    public int areaCuadrado(int a){
+        return (a*a);
+    }
+}
